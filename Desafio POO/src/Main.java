309a8c85d9e8f7d9a62bd82146f5bed95db8144a
richarlisonn <dev.richarlison@gmail.com>
2026-03-10 18:02:37 -0300

    Desafio concluido em 30min

--- Desafio POO/src/Main.java
+++ Desafio POO/src/Main.java
@@ -1,8 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
 
-        }
+        Pessoa inae = new Pessoa("Inae", 16, "SP");
+        Pessoa richarlison = new Pessoa("Richarlison", 18, "SP");
+
+        inae.aprensentar();
+        richarlison.aprensentar();
+    }
+}
+
+class Pessoa {
+    String nome;
+    int idade;
+    String cidade;
+
+    public Pessoa(String nome, int idade, String cidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cidade = cidade;
+    }
+
+    public void aprensentar(){
+        System.out.println("OLá, meu nome é " + nome + " Tenho " + idade + " anos e moro em " + cidade + "!");
     }
 }
